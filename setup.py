c260a4f98e227a0b90e9d19d1d96205335a1b87f
Marine Le Gall <marine.legall@alumni.esade.edu>
2026-02-20 18:19:38 +0100

    add find_packages()

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import find_packages
 from setuptools import setup
 
-with open("requirements.txt") as f:
-    content = f.readlines()
-requirements = [x.strip() for x in content if "git+" not in x]
+# with open("requirements.txt") as f:
+#     content = f.readlines()
+# requirements = [x.strip() for x in content if "git+" not in x]
 
 setup(name='ecologiedatagouvfr_PCAET',
       version="0.0.1",
@@ -25,7 +25,7 @@ from: https://www.territoires-climat.ademe.fr/actualite/comment-exploiter-les-do
       """,
       license="MIT",
       author_email="marine.le-gall@cerema.fr",
-      install_requires=requirements,
-      packages=["method"],
+    #   install_requires=requirements,
+      packages=find_packages(),
     #   test_suite="tests"
         )
