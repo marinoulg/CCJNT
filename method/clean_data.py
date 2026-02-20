12bbc05146a02f39c47125c912116a49c3b6feb3
Marine Le Gall <marine.legall@alumni.esade.edu>
2026-02-20 18:18:44 +0100

    update: clean_data up to date with new paths

--- method/clean_data.py
+++ method/clean_data.py
@@ -2,9 +2,9 @@ import os
 from params import *
 
 def clean_data_dir():
-    for elem in os.listdir(LOCAL_DATA_PATH):
-        if "emarche" not in elem:
-            os.remove(os.path.join(LOCAL_DATA_PATH,elem))
+    for elem in os.listdir(PATH_DATA_WIP):
+        os.remove(os.path.join(PATH_DATA_WIP,elem))
+    os.removedirs(PATH_DATA_WIP)
 
 if __name__ == "__main__":
     clean_data_dir()
