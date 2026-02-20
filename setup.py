cfb77e64a6d31389a38719efd5c77ed296c7cd04
Marine Le Gall <marine.legall@alumni.esade.edu>
2026-02-20 18:20:07 +0100

    update: add find_packages()

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 from setuptools import find_packages
 from setuptools import setup
 
-# with open("requirements.txt") as f:
-#     content = f.readlines()
-# requirements = [x.strip() for x in content if "git+" not in x]
-
 setup(name='ecologiedatagouvfr_PCAET',
       version="0.0.1",
       description="""
